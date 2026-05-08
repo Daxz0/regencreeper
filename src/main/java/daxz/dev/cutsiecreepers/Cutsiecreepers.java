03964e594fa54980d3193ba0b1462ac3652d1fec
Daxz <59543649+Daxz0@users.noreply.github.com>
2026-05-08 01:23:03 -0700

    forgot to register the event

--- src/main/java/daxz/dev/cutsiecreepers/Cutsiecreepers.java
+++ src/main/java/daxz/dev/cutsiecreepers/Cutsiecreepers.java
@@ -1,11 +1,14 @@
 package daxz.dev.cutsiecreepers;
 
+import daxz.dev.cutsiecreepers.Events.CreeperExplosionHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Cutsiecreepers extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        getServer().getPluginManager().registerEvents(new CreeperExplosionHandler(), this);
 
         saveResource("config.yml", true);
         saveDefaultConfig();
